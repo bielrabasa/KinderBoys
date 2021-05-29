cd190606ee16dcb249fa75ce3e89431b8d0dcbd9
Biel Rabasa <bielrabasa@gmail.com>
2021-05-29 20:58:23 +0200

    AnimacionsBOSS

--- Super Smash Tv/Source/ModuleBOSS.cpp
+++ Super Smash Tv/Source/ModuleBOSS.cpp
@@ -12,37 +12,61 @@
 ModuleBOSS::ModuleBOSS(bool startEnabled) : Module(startEnabled)
 {
 	IdleHead.Empty();
+	IdleHead.PushBack({ 32, 0, 32, 32 }); //3 iguals perquè duri més amb la boca tancada que oberta
 	IdleHead.PushBack({ 32, 0, 32, 32 });
-	IdleHead.PushBack({ 32, 32, 32, 32 });
+	IdleHead.PushBack({ 32, 0, 32, 32 });
+	IdleHead.PushBack({ 33, 32, 32, 32 });
 	IdleHead.loop = true;
-	IdleHead.speed = 0.5f;
+	IdleHead.speed = 0.01f;
 
 	RHead.Empty();
-	RHead.PushBack({ 64, 0, 32, 32 });
+	RHead.PushBack({ 65, 0, 30, 32 });
+	RHead.PushBack({ 65, 0, 30, 32 });
+	RHead.PushBack({ 65, 0, 30, 32 });
 	RHead.PushBack({ 64, 32, 32, 32 });
 	RHead.loop = true;
-	RHead.speed = 0.5f;
+	RHead.speed = 0.01f;
 
 	LHead.Empty();
-	LHead.PushBack({ 0, 0, 32, 32 });
-	LHead.PushBack({ 0, 32, 32, 32 });
+	LHead.PushBack({ -2, 0, 32, 32 });
+	LHead.PushBack({ -2, 0, 32, 32 });
+	LHead.PushBack({ -2, 0, 32, 32 });
+	LHead.PushBack({ -2, 32, 32, 32 });
 	LHead.loop = true;
-	LHead.speed = 0.5f;
+	LHead.speed = 0.01f;
 
 	Body.Empty();
-	Body.PushBack({ 0, 64, 128, 64 });
-	Body.PushBack({ 128, 64, 128, 64 });
-	Body.PushBack({ 0, 128, 128, 64 });
-	Body.PushBack({ 128, 128, 128, 64 });
+	Body.PushBack({ 256, 73, 128, 80 });	//1
+	Body.PushBack({ 256, 73, 128, 80 });	//1
+	Body.PushBack({ 256, 73, 128, 80 });	//1
+	Body.PushBack({ 256, 73, 128, 80 });	//1
+	Body.PushBack({ 256, 73, 128, 80 });	//1
+	Body.PushBack({ 256, 73, 128, 80 });	//1
+	Body.PushBack({ 256, 73, 128, 80 });	//1
+	Body.PushBack({ 256, 73, 128, 80 });	//1
+	Body.PushBack({ 256, 73, 128, 80 });	//1
+	Body.PushBack({ 256, 73, 128, 80 });	//1
+	Body.PushBack({ 256, 73, 128, 80 });	//1
+	Body.PushBack({ 256, 73, 128, 80 });	//1
+
+	Body.PushBack({ 384, 73, 128, 80 });	//2
+	Body.PushBack({ 0, 73, 128, 80 });		//3
+	Body.PushBack({ 128, 73, 128, 80 });	//4
+	Body.PushBack({ 128, 73, 128, 80 });	//4
+	Body.PushBack({ 0, 73, 128, 80 });		//3
+	Body.PushBack({ 384, 73, 128, 80 });	//2
+
+
+
 	Body.loop = true;
-	Body.speed = 0.1f;
+	Body.speed = 0.03f;
 
 	Wheels.Empty();
 	Wheels.PushBack({ 0, 192, 64, 96 });
 	Wheels.PushBack({ 64, 192, 64, 96 });
 	Wheels.PushBack({ 128, 192, 64, 96 });
 	Wheels.loop = true;
-	Wheels.speed = 0.1f;
+	Wheels.speed = 0.07f;
 
 	HeadAnimation = &IdleHead;
 	BodyAnimation = &Body;
@@ -65,7 +89,7 @@ bool ModuleBOSS::Start()
 
 	bool ret = true;
 
-	bgTexture = App->textures->Load("Assets/SpritesSSTV/Entity_BOSS.png");
+	bgTexture = App->textures->Load("Assets/SpritesSSTV/sprite_boss.png");
 
 	bossTimer = 0;
 
@@ -87,6 +111,11 @@ UpdateResult ModuleBOSS::Update()
 	//Treballar amb 'x' i 'y' del Boss
 
 
+
+	WheelsAnimation->Update();
+	BodyAnimation->Update();
+	HeadAnimation->Update();
+
 	return UpdateResult::UPDATE_CONTINUE;
 }
 
@@ -96,7 +125,7 @@ UpdateResult ModuleBOSS::PostUpdate()
 	App->render->DrawTexture(bgTexture, x, y, &RectWheels, 2); //no sumem ni restem
 	
 	SDL_Rect RectBody = BodyAnimation->GetCurrentFrame();
-	App->render->DrawTexture(bgTexture, x - 64, y - 48, &RectBody, 2); //x-64 y-48 centrat
+	App->render->DrawTexture(bgTexture, x - 70, y - 70, &RectBody, 2); //x-70 y-70 centrat
 	
 	SDL_Rect RectHead = HeadAnimation->GetCurrentFrame();
 	App->render->DrawTexture(bgTexture, x + 32, y - 64, &RectHead, 2); //x+32 y-64 centrat
